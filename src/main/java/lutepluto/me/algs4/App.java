388b95bd845f5cbc27339d55f767679837b2506f
lutepluto <lutepluto@gmail.com>
2017-06-25 16:03:55 +0800

    chore: scaffolding project

---
+++ src/main/java/lutepluto/me/algs4/App.java
@@ -0,0 +1,13 @@
+package lutepluto.me.algs4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
