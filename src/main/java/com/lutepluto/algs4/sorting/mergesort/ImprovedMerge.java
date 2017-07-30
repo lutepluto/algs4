707e10f326ae0a56e661ad00b4f11b637a6e9634
lutepluto <lutepluto@gmail.com>
2017-07-30 22:17:03 +0800

    add ImprovedMerge

---
+++ src/main/java/com/lutepluto/algs4/sorting/mergesort/ImprovedMerge.java
@@ -0,0 +1,69 @@
+/**
+ *
+ */
+package com.lutepluto.algs4.sorting.mergesort;
+
+import edu.princeton.cs.algs4.Insertion;
+
+/**
+ * Creative problem 2.2.11
+ *
+ * @author lute
+ */
+@SuppressWarnings("rawtypes")
+public class ImprovedMerge {
+
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+
+        // Use insertion sort for small subarrays
+        if (hi - lo + 1 <= 15) {
+            Insertion.sort(a, lo, hi);
+            return;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        // Test whether the array is already in order
+        if (less(a[mid], a[mid + 1])) {
+            return;
+        }
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[i], aux[j])) {
+                a[k] = aux[i++];
+            } else {
+                a[k] = aux[j++];
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+}
